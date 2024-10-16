ad39df19d51409ed8c0fead666eeaf468cc0a829
Sam Precious <samwdp@gmail.com>
2024-10-16 15:58:14 +0100

    fix: added nvim to symbolic links for windows

--- makesymlinks.ps1
+++ makesymlinks.ps1
@@ -1,7 +1,9 @@
 $wezterm = Resolve-Path "wezterm"
 $glaze = Resolve-Path "glazewm"
 $powershell = Resolve-Path "Powershell-Profile"
+$nvim = Resolve-Path "nvim"
 New-Item -Path ../.glzr -ItemType SymbolicLink -Value $glazewm
 New-Item -Path ../Documents/PowerShell -ItemType SymbolicLink -Value $powershell
 New-Item -Path ../.wezterm.lua -ItemType SymbolicLink -Value $wezterm
+New-Item -Path ..\AppData\Local\nvim -ItemType SymbolicLink -Value $nvim
 
