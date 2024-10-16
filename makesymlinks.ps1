b6384504688c05431b3d8e87b91cc957f88e0b34
Sam Precious <samwdp@gmail.com>
2024-10-16 14:50:48 +0100

    readme

--- makesymlinks.ps1
+++ makesymlinks.ps1
@@ -1,7 +1,7 @@
 $wezterm = Resolve-Path "wezterm"
 $glaze = Resolve-Path "glazewm"
-$powershell = Resolve-Path "Powershell"
+$powershell = Resolve-Path "PowerShell"
 New-Item -Path ../.glzr -ItemType SymbolicLink -Value $glazewm
-New-Item -Path ../Documents/PowerShell -ItemType Junction -Value $powershell
+New-Item -Path ../Documents/PowerShell -ItemType SymbolicLink -Value $powershell
 New-Item -Path ../.wezterm.lua -ItemType SymbolicLink -Value $wezterm
 
