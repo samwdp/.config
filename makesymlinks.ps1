44bc4499551f27a6c3ea5813bf0ce143a6109e71
Sam Precious <samwdp@gmail.com>
2024-11-06 14:45:48 +0000

    wezterm stuff

--- makesymlinks.ps1
+++ makesymlinks.ps1
@@ -5,6 +5,7 @@ $powershell = Resolve-Path "Powershell-Profile"
 $nvim = Resolve-Path "nvim"
 $emacs = Resolve-Path "emacs"
 $wezterm = Resolve-Path "wezterm/wezterm.lua.windows"
+$nushell = Resolve-Path "nushell"
 
 New-Item -Path ../.glzr -ItemType SymbolicLink -Value $glazewm
 New-Item -Path ../Documents/PowerShell -ItemType SymbolicLink -Value $powershell
@@ -12,5 +13,6 @@ New-Item -Path ../Documents/WindowsPowerShell -ItemType SymbolicLink -Value $pow
 New-Item -Path ../.wezterm.lua -ItemType SymbolicLink -Value $wezterm
 New-Item -Path ..\AppData\Local\nvim -ItemType SymbolicLink -Value $nvim
 New-Item -Path ..\AppData\Roamding\.emacs.d -ItemType SymbolicLink -Value $emacs
+New-Item -Path ..\AppData\Roamding\nushell -ItemType SymbolicLink -Value $nushell
 New-Item -Path wezterm/wezterm.lua -ItemType SymbolicLink -Value $wezterm
 
